cf206c8163af3405192b22c3595e3144adbc492b
Ilya Smal <ilyasmal@MacBook-Pro-Ilya.local>
2018-05-03 23:11:23 +0300

    Added indicators block

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,9 @@ import './../scss/style.scss';
 import MeVimeo from './components/meVimeo';
 import Header from './components/header';
 import Sandwich from './components/sandwich';
+import SlickSlider from './components/slickSlider';
 
 new MeVimeo().init();
 new Header().init();
 new Sandwich().init();
+new SlickSlider().init();
---
+++ src/js/components/slickSlider.js
@@ -0,0 +1,58 @@
+import $ from 'jquery';
+import 'slick-carousel';
+
+export default class Indicators {
+    constructor() {
+        this.sliderContainer = document.querySelector('.js-slick-slider-container')
+    }
+
+    init() {
+        if(!this.sliderContainer) return;
+
+        this.$sliderContainer = $(this.sliderContainer);
+        this.$slider = this.$sliderContainer.find('.js-slick-slider');
+        this.$arrows = this.$sliderContainer.find('.js-slick-slider-arrows');
+        this.$dots = this.$sliderContainer.find('.js-slick-slider-dots');
+
+        this.options = {
+            responsive: [
+                {
+                    breakpoint: 99999,
+                    settings: 'unslick'
+                },
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        adaptiveHeight: true,
+                        dots: true,
+                        appendDots: this.$dots,
+                        appendArrows: this.$arrows,
+                        prevArrow: this.getArrow('prev'),
+                        nextArrow: this.getArrow('next')
+                    }
+                }
+            ]
+        };
+
+        this.checkWindowSize();
+
+        window.addEventListener('resize', () => this.checkWindowSize());
+    }
+
+    getArrow(val) {
+        return `<button type="button" class="slick-slider__arrow _${val} js-slick-slider-${val}">${val}</button>`;
+    }
+
+    checkWindowSize() {
+        if(window.innerWidth < 1200) {
+            if(this.isSlicked) {
+                return
+            } else {
+                this.$slider.slick(this.options);
+                this.isSlicked = true;
+            }
+        } else {
+            this.isSlicked = false;
+        }
+    }
+}
